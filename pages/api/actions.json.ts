2049f6e3eec80f806cda17277129c5f6301ae541
David Mihal <david@dmihal.com>
2020-11-26 13:23:35 +0000

    Make actions endpoint with uniswap

---
+++ pages/api/actions.json.ts
@@ -0,0 +1,76 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { toChecksumAddress } from 'ethereum-checksum-address'
+
+const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+
+  const request = await fetch('https://api.thegraph.com/subgraphs/name/defi-777/kovan', {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `
+      query {
+        uniswapAdapters {
+          id
+          outputWrapper {
+            id
+            underlyingName
+            underlyingSymbol
+          }
+          outputPoolWrapper {
+            id
+            token0Name
+            token0Symbol
+            token0Address
+            token1Name
+            token1Symbol
+            token1Address
+          }
+        }
+      }`,
+      variables: null,
+    }),
+  })
+
+  const { data, errors } = await request.json()
+  if (errors) {
+    return res.status(500).json({ errors })
+  }
+
+  const uniswapAdapters = data.uniswapAdapters
+    .filter((adapter: any) => !adapter.outputPoolWrapper)
+    .map((adapter: any) => ({
+      address: toChecksumAddress(adapter.id),
+      outputWrapper: toChecksumAddress(adapter.outputWrapper.id),
+      name: adapter.outputWrapper.underlyingName,
+      symbol: adapter.outputWrapper.underlyingSymbol,
+    }))
+
+  const uniswapPoolAdapters = data.uniswapAdapters
+    .filter((adapter: any) => adapter.outputPoolWrapper)
+    .map(({ outputPoolWrapper, id }: any) => ({
+      address: toChecksumAddress(id),
+      outputWrapper: toChecksumAddress(outputPoolWrapper.id),
+      name: `Uniswap ${outputPoolWrapper.token0Name}-${outputPoolWrapper.token1Name} Pool`.replace('Wrapped Ether', 'Ether'),
+      symbol: `UNI-${outputPoolWrapper.token0Symbol}/${outputPoolWrapper.token1Symbol}`.replace('WETH', 'ETH'),
+    }))
+
+  res.json({
+    actions: [
+      {
+        name: 'Uniswap',
+        description: 'Swap for other tokens',
+        adapters: uniswapAdapters,
+      },
+      {
+        name: 'Uniswap Pools',
+        description: 'Provide liquidity & earn trading fees',
+        adapters: uniswapPoolAdapters,
+      },
+    ],
+  })
+}
+
+export default handler
